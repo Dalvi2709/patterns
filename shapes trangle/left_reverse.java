25ce1a339690567b9307461af60d180c60da12a0
Roshan Dalvi <roshandalvi16@gmail.com>
2025-07-27 23:29:34 +0530

    left_reverse angle trangle

--- shapes trangle/left_reverse.java
+++ shapes trangle/left_reverse.java
@@ -1,9 +1,9 @@
+public class left_reverse {
 
-public class left {
     public static void main(String[] args) {
         int n = 5;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n-i; j++) {
                 System.out.print("  ");
             }
 
